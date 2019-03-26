da6c3c12e6210d0ed44d149aa768d5f90e0cdeaa
Jhia Turner <jhimitu@gmail.com>
2019-03-26 15:27:01 -0700

    add answer validation for correct input

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict';
 
-let morningPersonAnswer = prompt('Am I a morning person?');
-let orangeSodaAnswer = prompt('Do I like orange soda?');
-let cheesyDanceMovieAnswer = prompt('Do I love cheesy dance movies?');
-let tallerThanAnswer = prompt('Am I taller than 5\'3\"?');
-let playsInstrumentAnswer = prompt('Do I play an instrument?');
+let morningPersonAnswer = prompt('Am I a morning person?').toLowerCase();
+
+if (morningPersonAnswer === 'yes') {
+  alert('Yes! That\'s correct! My brain works much better in the morning.');
+}
+let orangeSodaAnswer = prompt('Do I like orange soda?').toLowerCase();
+if (orangeSodaAnswer === 'no') {
+  alert('Correct! I actually hate orange-flavored anything...unless it\'s an actual orange.');
+}
+let cheesyDanceMovieAnswer = prompt('Do I love cheesy dance movies?').toLowerCase();
+if (cheesyDanceMovieAnswer === 'yes') {
+  alert('You guessed it! Top faves include: You Got Served, Honey, and Stomp The Yard');
+}
+let tallerThanAnswer = prompt('Am I taller than 5\'3\"?').toLowerCase();
+if (tallerThanAnswer === 'no') {
+  alert('You\'re right. I\'m actually the shortest in my immediate family. And they remind me of this A LOT.')
+}
+let playsInstrumentAnswer = prompt('Do I play an instrument?').toLowerCase();
+if (playsInstrumentAnswer === 'yes') {
+  alert('Yep! I play guitar, and I also sing. Fun fact: at one point I was part of my family band. Until I moved away.');
+}
