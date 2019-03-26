2ffccd7ed07d29958152067aa78bb3fa69c58396
Jhia Turner <jhimitu@gmail.com>
2019-03-26 14:57:48 -0700

    add five prompts for user

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 'use strict';
+
+let morningPersonAnswer = prompt('Am I a morning person?');
+let orangeSodaAnswer = prompt('Do I like orange soda?');
+let cheesyDanceMovieAnswer = prompt('Do I love cheesy dance movies?');
+let tallerThanAnswer = prompt('Am I taller than 5\'3\"?');
+let playsInstrumentAnswer = prompt('Do I play an instrument?');
